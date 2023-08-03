d7efb4ee03663abcf77a5e2ce66eb29371bcb62e
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-08-03 11:51:33 -0300

    Created commit control and autoclose

--- src/loja_virtual_repository/TestaInsercaoComParametro.java
+++ src/loja_virtual_repository/TestaInsercaoComParametro.java
@@ -9,27 +9,40 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 	
 	public static void main(String[] args) throws SQLException {
-		String nome = "Testando parametro'";
-		String descricao = "Descricao teste parametro";
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		
-		PreparedStatement stm = 
-				connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-		
+		try(Connection connection = factory.recuperarConexao()) {
+			connection.setAutoCommit(false);
+			
+			try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+							Statement.RETURN_GENERATED_KEYS)){
+	
+				
+				adicionarVariavel("Smart TV", "45 polegadas", stm);
+				adicionarVariavel("Rádio", "Radio à bateria", stm);
+				
+				connection.commit();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
+		};
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 		
 		stm.execute();
 		
-		ResultSet rst = stm.getGeneratedKeys();
-		
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while(rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
+			
 		}
-		
 	}
 
 }
