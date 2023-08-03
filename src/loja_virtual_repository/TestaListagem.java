306a50cc815e22ce83d3668d646f083686aea740
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-08-03 10:15:08 -0300

    Created avoid for SQL injection

--- src/loja_virtual_repository/TestaListagem.java
+++ src/loja_virtual_repository/TestaListagem.java
@@ -1,9 +1,9 @@
 package loja_virtual_repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaListagem {
 	
@@ -12,11 +12,9 @@ public class TestaListagem {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperarConexao();
 		
-		Statement stm = connection.createStatement();
-		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
-		
+		PreparedStatement stm = connection.prepareStatement("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		stm.execute();
 		ResultSet rst = stm.getResultSet();
-		
 		while(rst.next()) {
 			Integer id = rst.getInt("ID");
 			String nome = rst.getString("NOME");
